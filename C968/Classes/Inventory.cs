eb70ee85b7121ee8cf521715040c4905058cc26b
mistahoward <me@alexhoward.dev>
2023-01-29 21:33:46 -0600

    Finalizing scaffolding - shaping out the inventory object

--- C968/Classes/Inventory.cs
+++ C968/Classes/Inventory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,40 @@ namespace C968.Classes
 {
     class Inventory
     {
+        public static BindingList<Product> Products = new BindingList<Product>();
+        public static BindingList<Part> Parts = new BindingList<Part>();
         
+        public void AddProduct(Product productToAdd)
+        {
+
+        }
+        public bool RemoveProduct(int productIdToRemove)
+        {
+        }
+        public Product LookupProduct(int productIdToLookup)
+        {
+
+        }
+        public void UpdateProduct(int productIdToUpdate, Product productInfo)
+        {
+
+        }
+        public void AddPart(Part partToAdd)
+        {
+
+        }
+        public bool DeletePart(Part partToDelete)
+        {
+
+        }
+        public Part LookupPart(int partIdToLookup)
+        {
+
+        }
+        public void UpdatePart(int partdIdToUpdate, Part partInfo)
+        {
+
+        }
+
     }
 }
